1030aa6867b0e2da5fee27503b7cf1e209458f60
Shermanico <jnjimenezmid@protonmail.com>
2026-02-09 21:57:46 -0600

    Front End initial files in development

---
+++ Front.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello and welcome to the Cnturion Inventory App\n");
+	printf("Here's a list of the options that you can do:\n");
+	printf("1- Add Product\n");
+	printf("2- Update Product\n");
+	printf("3- Remove Product\n");
+	int option;
+
+	switch (option) {
+ case 1:
+	printf("It works");
+	break;
+
+ default:
+	break;
+ }
+}
+
